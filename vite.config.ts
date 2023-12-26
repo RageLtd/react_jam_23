d4b23b365fffc02ca6b6985d0b63608bb75d0e7d
Gary M <exkage@yahoo.com>
2023-12-26 13:54:35 -0800

    Use happy-dom for vitest (#7)
    * UnoCSS fixes
    
    * Tailwind Compat Style Reset
    
    * Use happy-dom for vitest
    
    ---------
    
    Co-authored-by: Gary McWhorter <gary@nextshift.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
 import react from "@vitejs/plugin-react-swc";
@@ -5,4 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [UnoCSS(), react()],
+  test: {
+    environment: "happy-dom",
+  },
 });
